ff01ce0ec4c6ca887ba29cb482d999203552483b
vishalsolanki <vishalsolanki4880@gmail.com>
2025-05-23 12:13:40 +0530

    update

---
+++ src/Components/About_Page/Our_services/Our_services.jsx
@@ -0,0 +1,13 @@
+import "./Our_services.css"
+
+function Our_services() {
+    return (
+        <>
+            <div className="our_services">
+
+            </div>
+        </>
+    )
+}
+
+export default Our_services;
