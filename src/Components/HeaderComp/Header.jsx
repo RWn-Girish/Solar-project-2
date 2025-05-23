88b64cfded9a2846c56904a350fd90df1c6816d1
vishalsolanki <vishalsolanki4880@gmail.com>
2025-05-23 11:41:00 +0530

    update

--- src/Components/HeaderComp/Header.jsx
+++ src/Components/HeaderComp/Header.jsx
@@ -10,43 +10,43 @@ import { CgMoreVerticalO } from "react-icons/cg";
 import "./header.css";
 
 function Header() {
-  return (
-    <>
-   <div className="container ">
-      {[false].map((expand) => (
-        <Navbar key={expand} expand={expand} className="">
-          <Container className='position-absolute header-wrapper'>
-            <Navbar.Brand href="#"> <img src={logo} alt=""  className='img-fluid'/></Navbar.Brand>
-            <Navbar.Toggle><CgMoreVerticalO  className='navbar-toggler-icons'/>
-            </Navbar.Toggle>
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                        <img src={logo} alt="" />
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">About Us</Nav.Link>
-                  <Nav.Link href="#action2">Services</Nav.Link>
-                  <Nav.Link href="#action2">Pakages</Nav.Link>
-                  <Nav.Link href="#action2">Our Projects</Nav.Link>
-                  <Nav.Link href="#action2">Blog</Nav.Link>
-                   <Button className="header-btn">Contact Us</Button>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}  
-   </div>
-    </>
-  );
+    return (
+        <>
+            <div className="container ">
+                {[false].map((expand) => (
+                    <Navbar key={expand} expand={expand} className="">
+                        <Container className='position-absolute header-wrapper'>
+                            <Navbar.Brand href="#"> <img src={logo} alt="" className='img-fluid' /></Navbar.Brand>
+                            <Navbar.Toggle><CgMoreVerticalO className='navbar-toggler-icons' />
+                            </Navbar.Toggle>
+                            <Navbar.Offcanvas
+                                id={`offcanvasNavbar-expand-${expand}`}
+                                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                                placement="end"
+                            >
+                                <Offcanvas.Header closeButton>
+                                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                        <img src={logo} alt="" />
+                                    </Offcanvas.Title>
+                                </Offcanvas.Header>
+                                <Offcanvas.Body>
+                                    <Nav className="justify-content-end flex-grow-1 pe-3">
+                                        <Nav.Link href="#action1">Home</Nav.Link>
+                                        <Nav.Link href="#action2">About Us</Nav.Link>
+                                        <Nav.Link href="#action2">Services</Nav.Link>
+                                        <Nav.Link href="#action2">Pakages</Nav.Link>
+                                        <Nav.Link href="#action2">Our Projects</Nav.Link>
+                                        <Nav.Link href="#action2">Blog</Nav.Link>
+                                        <Button className="header-btn">Contact Us</Button>
+                                    </Nav>
+                                </Offcanvas.Body>
+                            </Navbar.Offcanvas>
+                        </Container>
+                    </Navbar>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default Header;
