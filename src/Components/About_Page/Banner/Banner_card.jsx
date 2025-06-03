605777480bae97bb83f3bc7f50ceb9e28fcacc42
vishal <vishalsolanki4880@gmail.com>
2025-06-03 11:08:44 +0530

    banner update

--- src/Components/About_Page/Banner/Banner_card.jsx
+++ src/Components/About_Page/Banner/Banner_card.jsx
@@ -3,10 +3,10 @@ import "../../../Components/About_Page/Banner/Banner_card.css"
 function Banner_card({ title, image, description }) {
     return (
         <div className="banner_card p-4">
-            <div className="card_text d-flex mb-3 ">
+            <div className="card_text d-flex justify-content-center p-3">
                 {image}
-                <h2 className="fw-bold m-0">{title}</h2>
             </div>
+                <h2 className="fw-bold m-0">{title}</h2>
             <p>{description}</p>
         </div>
     );
