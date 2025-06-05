f408231da7718dfe2e39967c58b452787cc91560
vishal <vishalsolanki4880@gmail.com>
2025-06-05 11:43:00 +0530

    Our_esteemed update

--- src/Components/About_Page/Our_esteemed/Our_esteemed.jsx
+++ src/Components/About_Page/Our_esteemed/Our_esteemed.jsx
@@ -1,6 +1,24 @@
 import "../Our_esteemed/Our_esteemed.css"
 import { Container } from "react-bootstrap";
-import logo from "../../../assets/images/logo.png"
+import Esteemed1 from "../../../assets/images/Esteemed1.png"
+import Esteemed2 from "../../../assets/images/Esteemed2.png"
+import Esteemed3 from "../../../assets/images/Esteemed3.png"
+import Esteemed4 from "../../../assets/images/Esteemed4.png"
+import Esteemed5 from "../../../assets/images/Esteemed5.png"
+import Esteemed6 from "../../../assets/images/Esteemed6.png"
+import Esteemed7 from "../../../assets/images/Esteemed7.png"
+import Esteemed8 from "../../../assets/images/Esteemed8.png"
+import Esteemed9 from "../../../assets/images/Esteemed9.png"
+import Esteemed10 from "../../../assets/images/Esteemed10.png"
+import Esteemed11 from "../../../assets/images/Esteemed11.png"
+import Esteemed12 from "../../../assets/images/Esteemed12.png"
+import Esteemed14 from "../../../assets/images/Esteemed14.png"
+import Esteemed15 from "../../../assets/images/Esteemed15.png"
+import Esteemed16 from "../../../assets/images/Esteemed16.png"
+import Esteemed17 from "../../../assets/images/Esteemed17.png"
+import Esteemed18 from "../../../assets/images/Esteemed18.png"
+import Esteemed19 from "../../../assets/images/Esteemed19.png"
+import Esteemed20 from "../../../assets/images/Esteemed20.png"
 
 function Our_esteemed() {
     return (
@@ -8,27 +26,27 @@ function Our_esteemed() {
             <Container>
                 <div className="Our_esteemed d-flax gap-5">
                     <h1>Our <span>Esteemed Clients and Partners</span><br />through the Years.</h1>
-                    <div className="row">
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
-                        <div className="col-2"><img src={logo} alt="" /></div>
+                    <div className="row mt-5 mb-5 d-flex gap-5">
+                        <div className="col-2"><img src={Esteemed1} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed2} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed3} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed4} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed5} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed6} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed7} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed8} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed9} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed10} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed11} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed12} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed12} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed14} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed15} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed16} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed17} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed18} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed19} alt="" /></div>
+                        <div className="col-2"><img src={Esteemed20} alt="" /></div>
                     </div>
                 </div >
             </Container>
